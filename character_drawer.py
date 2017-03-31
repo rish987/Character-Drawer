af34568e26a6987090f12e648ca2ea21fd30c874
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2017-03-30 18:28:58 -0700

    Implement character drawing and clearing the canvas.

--- character_drawer.py
+++ character_drawer.py
@@ -7,7 +7,7 @@ Created:
 24/03/2017
 
 Last Modified:
-Thu 30 Mar 2017 10:22:25 AM PDT
+Thu 30 Mar 2017 06:27:56 PM PDT
 
 Description:
 This program provides an easy-to-use environment where a user can draw a symbol
@@ -43,7 +43,8 @@ from left to right and top to bottom.
 from Tkinter import *
 
 # window size (both width and height)
-WINDOW_SIZE = 300;
+WINDOW_WIDTH = 250;
+WINDOW_HEIGHT = 300;
 
 # padding of all components inside of window
 PADDING = 20
@@ -54,24 +55,39 @@ DC_MAX_SIZE = 100;
 
 # drawing canvas size (both width and height)
 # NOTE: must evenly divide DC_SIZE_PX
-DC_SIZE = 50;
+DC_SIZE = 20;
 
 # drawing canvas size in pixels (both width and height)
 DC_SIZE_PX = 200;
 
+# bit size in pixels (both width and height)
+BIT_SIZE_PX = DC_SIZE_PX / DC_SIZE;
+
 # currently entered character
 char = '';
 
+# is the mouse currently being pressed?
+mouse_pressed = False;
+
+# the drawing canvas
+drawing_canvas = 0;
+
+# the drawing
+drawing = [ [ False for col in range( 0, DC_SIZE ) ]
+              for row in range( 0, DC_SIZE ) ];
 
 def main ():
     """
     Sets up the canvas for drawing and using this program.
     """
+    global drawing_canvas;
+    global drawing;
+
     # set up the window with appropriate dimensions 
     window = Tk();
     window.wm_title( "Character Drawer" );
     window.resizable( width=False, height=False );
-    window.geometry( '{}x{}'.format( WINDOW_SIZE, WINDOW_SIZE ) );
+    window.geometry( '{}x{}'.format( WINDOW_WIDTH, WINDOW_HEIGHT ) );
     window.configure( background='white' );
 
     # --- create the drawing canvas with its grid ---
@@ -84,26 +100,11 @@ def main ():
     # load the drawing canvas
     drawing_canvas.pack( padx=PADDING, pady=PADDING );
 
-    # number of lines to draw on the grid
-    num_lines = DC_SIZE - 1;
+    redraw_canvas();
 
-    # offset of lines to draw on the grid
-    lines_offset = DC_SIZE_PX / DC_SIZE;
-
-    # go through all gridlines to be drawn
-    for line_i in range( 0, num_lines ):
-        # the x and y pixel position of the line to be drawn
-        pos = ( line_i + 1 ) * lines_offset;
-
-        # draw the x and y gridlines
-        drawing_canvas.create_line( 1, pos + 1, DC_SIZE_PX, pos + 1,
-            fill='light grey' );
-        drawing_canvas.create_line( pos + 1, 1, pos + 1, DC_SIZE_PX, 
-            fill='light grey' );
-
-    # draw the bounding rectangle of the grid
-    drawing_canvas.create_rectangle( 1, 1, DC_SIZE_PX + 1, DC_SIZE_PX + 1,
-        outline='black' );
+    drawing_canvas.bind( "<Motion>", move_mouse )
+    drawing_canvas.bind( "<ButtonPress-1>", press_mouse )
+    drawing_canvas.bind( "<ButtonRelease-1>", release_mouse )
 
     # ---
 
@@ -131,15 +132,24 @@ def main ():
  
     # ---
 
+    # --- add buttons to: save image, clear canvas ---
+
     # frame to hold control panel widgets
     control_panel = Frame( window );
 
-    # TODO add buttons to: save image, clear canvas
+    # add save button
+    save_btn = Button( control_panel, text='Save', command=save_button );
+    save_btn.pack( side=LEFT );
+    
+    # add save button
+    clear_btn = Button( control_panel, text='Clear', command=clear_button );
+    clear_btn.pack( side=LEFT );
 
     # display the control panel
     control_panel.pack();
 
-    # TODO set up event listeners
+    # ---
+
     # TODO set up the necessary folders
     # TODO TODO anymore setup that needs to be done
 
@@ -164,9 +174,7 @@ def char_callback ( entered ):
 
         # set the entered character to its own first character
         entered.set( char );
-    print char;
 
-# TODO TODO write event listeners
 def press_mouse ( event ):
     """
     Handles pressing the mouse button by turning on a boolean that lets the
@@ -175,7 +183,12 @@ def press_mouse ( event ):
     Parameters: 
     event - the event that triggered this method
     """
-    # TODO turn on mouse pressed indicator boolean
+    # turn on mouse pressed indicator boolean
+    global mouse_pressed;
+    mouse_pressed = True;
+
+    # draw at this location
+    draw_at( event.x, event.y );
 
 def release_mouse ( event ):
     """
@@ -185,30 +198,118 @@ def release_mouse ( event ):
     Parameters:
     event - the event that triggered this method
     """
-    # TODO turn off mouse pressed indicator boolean
+    # turn off mouse pressed indicator boolean
+    global mouse_pressed;
+    mouse_pressed = False;
 
-def clear_button ( event ):
+def move_mouse ( event ):
+    """
+    Handles moving the mouse button by drawing on the canvas if the mouse
+    button is being pressed.
+
+    Parameters:
+    event - the event that triggered this method
+    """
+    # the mouse is being pressed
+    if mouse_pressed:
+        # draw at this location
+        draw_at( event.x, event.y );
+
+def clear_button ():
     """ 
     Handles the user pressing the clear button by clearing their drawing
     from the screen.
-
-    Parameters:
-    event - the event that triggered this method
     """
-    # TODO TODO clear the drawing
+    global drawing;
 
-def save_button ( event ):
+    # clear the drawing
+    drawing = [ [ False for col in range( 0, DC_SIZE ) ]
+                  for row in range( 0, DC_SIZE ) ];
+
+    redraw_canvas();
+
+def save_button ():
     """
     Handles the user pressing the save button by saving their drawing's image
     and data to file.
-
-    Parameters:
-    event - the event that triggered this method
     """
     # TODO check that the text field has one character in it to associate with
     # this drawing
     # TODO TODO save the drawing's data
     # TODO TODO save the drawing's image
+
+def draw_at ( x_loc, y_loc ):
+    """
+    Draws at the given x- and y-locations on the drawing canvas
+
+    Parameters:
+    x_loc - the x-location to draw at
+    y_loc - the y-location to draw at
+    """
+    global drawing_canvas;
+    global drawing;
+
+    x_loc = x_loc - 2;
+    y_loc = y_loc - 2;
+
+    # the x- or y-location is out of range
+    if x_loc >= DC_SIZE_PX or y_loc >= DC_SIZE_PX or x_loc < 0 or y_loc < 0:
+        return;
+
+    # x- and y-indices in the drawing to set
+    x_ind = x_loc / BIT_SIZE_PX;
+    y_ind = y_loc / BIT_SIZE_PX;
+
+    # set this bit in the drawing
+    drawing[ x_ind ][ y_ind ] = True;
+
+    redraw_canvas();
+
+def redraw_canvas ():
+    """
+    Redraws to canvas to fill in the boxes specified by the drawing.
+    """
+    global drawing_canvas;
+    global drawing;
+
+    # color the background
+    drawing_canvas.create_rectangle( 1, 1, DC_SIZE_PX + 1, DC_SIZE_PX + 1,
+        fill='white' );
+
+    # number of lines to draw on the grid
+    num_lines = DC_SIZE - 1;
+
+    # offset of lines to draw on the grid
+    lines_offset = BIT_SIZE_PX;
+
+    # go through all gridlines to be drawn
+    for line_i in range( 0, num_lines ):
+        # the x and y pixel position of the line to be drawn
+        pos = ( line_i + 1 ) * lines_offset;
+
+        # draw the x and y gridlines
+        drawing_canvas.create_line( 1, pos + 1, DC_SIZE_PX, pos + 1,
+            fill='light grey' );
+        drawing_canvas.create_line( pos + 1, 1, pos + 1, DC_SIZE_PX, 
+            fill='light grey' );
+
+    # draw the bounding rectangle of the grid
+    drawing_canvas.create_rectangle( 1, 1, DC_SIZE_PX + 1, DC_SIZE_PX + 1,
+        outline='black' );
+
+    # go through all of the rows in the drawing
+    for row in range( 0, len( drawing ) ):
+        # go through all of the columns in this row of the drawing
+        for col in range( 0, len( drawing[ row ] ) ):
+            # this bit is on
+            if drawing[ row ][ col ]:
+                # draw this bit
+                drawing_canvas.create_rectangle( 
+                    BIT_SIZE_PX * row + 2, 
+                    BIT_SIZE_PX * col + 2, 
+                    BIT_SIZE_PX * row + BIT_SIZE_PX,
+                    BIT_SIZE_PX * col + BIT_SIZE_PX,
+                    fill='black');
 
 # start the program
 main();
